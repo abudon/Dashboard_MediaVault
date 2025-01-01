e065630e6c5048446b891039803e025f5453cd45
abudon <abrahamabudon@gmail.com>
2025-01-01 23:47:59 +0100

    modifications_010120252347

--- src/context/loggingConxtext.js
+++ src/context/loggingConxtext.js
@@ -1,35 +1,29 @@
-import {createContext, useContext, useState} from 'react'
+import { createContext, useContext, useState } from "react";
 
+const login_context = createContext(null);
 
-const login_context = createContext(null)
-
-
-const LoginProvider = ({children}) =>{
-    const [username, setUsername] = useState(()=>{
+const LoginProvider = ({ children }) => {
+    const [username, setUsername] = useState(() => {
         // Check if the user is logged in from local storage
-        const savedUsername =JSON.parse(localStorage.getItem('admin')).username;
+        const savedAdmin = localStorage.getItem("admin");
+        const savedUsername = savedAdmin ? JSON.parse(savedAdmin)?.username : null;
 
-        return savedUsername? savedUsername : null;
+        return savedUsername ? savedUsername : " ";
     });
 
-
-    return(
-        <login_context.Provider value={{username, setUsername}}>
+    return (
+        <login_context.Provider value={{ username, setUsername }}>
             {children}
         </login_context.Provider>
-    )
-}
+    );
+};
 
-const useLoginContext = ()=>{
-
+const useLoginContext = () => {
     const context = useContext(login_context);
     if (!context) {
-        throw new Error("usePhotoLabContext should be used inside the PhotoLabContextProvider.");
+        throw new Error("useLoginContext must be used within a LoginProvider.");
     }
-
     return context;
-    }
+};
 
-    export {
-    LoginProvider, useLoginContext
-    }
+export { LoginProvider, useLoginContext };
